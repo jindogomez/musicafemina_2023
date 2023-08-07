e2f8ed80844e3a57a12426bda7e649abfe787364
l4ndl0s1 <32853958+l4ndl0s1@users.noreply.github.com>
2023-08-07 16:53:39 +0200

    backer import änderung

--- lib/Pages/map_baker.dart
+++ lib/Pages/map_baker.dart
@@ -7,6 +7,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 //file import
 import '../MapContent/All/waypoint_images.dart';
+import '../MapContent/Baker/backer_polylines.dart';
 import '../Services/constants_mapbox.dart';
 import '../Services/location_helper.dart';
 
@@ -14,7 +15,7 @@ import '../Widgets/appbar_maps.dart';
 import '../Widgets/center_floatingbutton.dart';
 import '../MapContent/Baker/baker_marker.dart';
 import '../Services/directions_service.dart';
-import '../MapContent/Baker/baker_polylines.dart';
+
 import '../Widgets/marker_card_baker.dart';
 
 typedef UpdateCallback = void Function(void Function());
